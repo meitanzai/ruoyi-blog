eeb565da99057383e25eb088f959e7a87fd97cb4
1228134035@qq.com <1228134035@qq.com>
2022-04-29 21:43:00 +0800

    修改显示顺序orderNum类型为整型

--- src/main/java/com/ruoyi/project/system/menu/domain/Menu.java
+++ src/main/java/com/ruoyi/project/system/menu/domain/Menu.java
@@ -29,7 +29,7 @@ public class Menu extends BaseEntity
     private Long parentId;
 
     /** 显示顺序 */
-    private String orderNum;
+    private Integer orderNum;
 
     /** 菜单URL */
     private String url;
@@ -97,13 +97,13 @@ public class Menu extends BaseEntity
         this.parentId = parentId;
     }
 
-    @NotBlank(message = "显示顺序不能为空")
-    public String getOrderNum()
+    @NotNull(message = "显示顺序不能为空")
+    public Integer getOrderNum()
     {
         return orderNum;
     }
 
-    public void setOrderNum(String orderNum)
+    public void setOrderNum(Integer orderNum)
     {
         this.orderNum = orderNum;
     }
